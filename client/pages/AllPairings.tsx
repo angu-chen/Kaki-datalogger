af120731f884e6071242c3b97b452756364efebf
Angu Chen <anguchen96@gmail.com>
2026-01-16 12:10:03 +1300

    added a new pairing button

--- client/pages/AllPairings.tsx
+++ client/pages/AllPairings.tsx
@@ -1,9 +1,14 @@
 import AllPairingsTable from '../components/AllPairingsTable'
+import AddBut from '../components/buttons/AddBut'
+import PairingForm from '../components/forms/PairingForm'
 
 export function AllPairings() {
   return (
     <div className="flex flex-col items-center h-screen">
       <h1 className="text-4xl my-5 font-semibold"> All Kakī Pairings</h1>
+      <AddBut Form={PairingForm}>
+        <p>New Pairing</p>
+      </AddBut>
       <div className="w-4/5 my-2 overflow-scroll h-2/3">
         <AllPairingsTable />
       </div>
