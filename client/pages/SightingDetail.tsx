8e6167ad2ad8aeac2b0f4fd7ad8d2ce24b5269fc
Angu Chen <anguchen96@gmail.com>
2026-01-23 14:35:08 +1300

    cleaned up sighting UI

--- client/pages/SightingDetail.tsx
+++ client/pages/SightingDetail.tsx
@@ -3,6 +3,7 @@ import { useDelSightingMutation, useSightingbyId } from '../hooks/useKaki'
 import DelBut from '../components/buttons/DelBut'
 import EditBut from '../components/buttons/EditBut'
 import SightingForm from '../components/forms/SightingForm'
+import { Map } from '../components/maps/Map'
 
 export default function SightingsDetail() {
   const params = useParams()
@@ -20,50 +21,70 @@ export default function SightingsDetail() {
 
   if (isLoading) return <h1> Looking for Kakī</h1>
   if (!sightingData) return <p> No Data</p>
+  // creating map props//
+  const mapData = [
+    {
+      id: sightingData.id,
+      x: sightingData.x,
+      y: sightingData.y,
+      msg: `Kakī ${sightingData.band} was seen at ${sightingData.area} by ${sightingData.observer} on ${sightingData.date}`,
+    },
+  ]
 
   return (
-    <div className="">
-      <div className="flex gap-10 flex-wrap m-5">
-        <div>
-          <h3 className="font-semibold text-sm">Date</h3>
-          <p>{sightingData.date}</p>
-        </div>
-        <button
-          className="cursor-pointer hover:text-B"
-          onClick={() => navigate(`/${sightingData.birdId}`)}
-        >
-          <h3 className="font-semibold text-sm ">Kaki Band</h3>
-          <p>{sightingData.band}</p>
-        </button>
-        <div>
-          <h3 className="font-semibold text-sm">Area</h3>
-          <p>{sightingData.area}</p>
-        </div>
-        <div>
-          <h3 className="font-semibold text-sm">Location</h3>
-          <p>{sightingData.location}</p>
-        </div>
-        <div>
-          <h3 className="font-semibold text-sm">Easting</h3>
-          <p>{sightingData.x}</p>
-        </div>
-        <div>
-          <h3 className="font-semibold text-sm">Northing</h3>
-          <p>{sightingData.y}</p>
-        </div>
-        <div>
-          <h3 className="font-semibold text-sm">Observer</h3>
-          <p>{sightingData.observer}</p>
-        </div>
-        <div>
-          <h3 className="font-semibold text-sm">Notes</h3>
-          <p>{sightingData.notes}</p>
+    <div className="flex flex-col items-center">
+      <div className="my-5">
+        <h1 className="text-4xl font-semibold text-center">{`Sighting no.${sightingData.id} Kakī ${sightingData.band}`}</h1>
+        <div className="flex gap-3 my-3 justify-center">
+          <EditBut editData={sightingData} Form={SightingForm} />
+
+          <DelBut id={Number(sightingData.id)} mutationFn={delSighting} />
         </div>
       </div>
-      <EditBut editData={sightingData} Form={SightingForm}>
-        Edit
-      </EditBut>
-      <DelBut id={Number(sightingData.id)} mutationFn={delSighting} />
+      <div className="flex flex-col gap-3 items-center w-5/6">
+        <div className="flex gap-5 text-sm flex-wrap justify-center w-full">
+          <div>
+            <h3 className="font-semibold text-sm">Date</h3>
+            <p>{sightingData.date}</p>
+          </div>
+          <div>
+            <h3 className="font-semibold text-sm ">Kaki Band</h3>
+            <button
+              className="cursor-pointer hover:text-B"
+              onClick={() => navigate(`/${sightingData.birdId}`)}
+            >
+              <p>{sightingData.band}</p>
+            </button>
+          </div>
+          <div>
+            <h3 className="font-semibold text-sm">Area</h3>
+            <p>{sightingData.area}</p>
+          </div>
+          <div>
+            <h3 className="font-semibold text-sm">Location</h3>
+            <p>{sightingData.location}</p>
+          </div>
+          <div>
+            <h3 className="font-semibold text-sm">Easting</h3>
+            <p>{sightingData.x}</p>
+          </div>
+          <div>
+            <h3 className="font-semibold text-sm">Northing</h3>
+            <p>{sightingData.y}</p>
+          </div>
+          <div>
+            <h3 className="font-semibold text-sm">Observer</h3>
+            <p>{sightingData.observer}</p>
+          </div>
+          <div>
+            <h3 className="font-semibold text-sm">Notes</h3>
+            <p>{sightingData.notes}</p>
+          </div>
+        </div>
+        <div className="w-full">
+          <Map data={mapData} />
+        </div>
+      </div>
     </div>
   )
 }
