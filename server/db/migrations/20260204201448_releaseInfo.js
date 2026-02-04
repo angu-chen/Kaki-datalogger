14f5c24640cbd13d2edbe9c7891a5d6080697a5b
Angu Chen <anguchen96@gmail.com>
2026-02-05 09:56:30 +1300

    added release info table into migrations

---
+++ server/db/migrations/20260204201448_releaseInfo.js
@@ -0,0 +1,25 @@
+import knex from 'knex'
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable('release_info', (table) => {
+    table.increments('id')
+    table.integer('bird_id')
+    table.boolean('feeding')
+    table.boolean('seen')
+    table.string('notes')
+    table.date('date')
+    table.integer('release_id')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.SchemaBuilder.dropTable('release_info')
+}
