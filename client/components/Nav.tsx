572e8af5953dce172541f97bf2b79315a8899c06
Angu Chen <anguchen96@gmail.com>
2026-02-18 15:42:28 +1300

    added a kaki logo and some nav ui

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,19 +1,24 @@
+import { href } from 'react-router'
+import { link } from 'superagent'
+
 export default function Nav() {
+  const navLinks = [
+    { name: 'Home', href: '/' },
+    { name: 'Pairings', href: '/pairings' },
+    { name: 'Sightings', href: '/sightings' },
+    { name: 'Releases', href: '/releases' },
+  ]
   return (
-    <nav className="mx-5 font-semibold text-md" role="navigation">
+    <nav className="mx-6 font-semibold text-md" role="navigation">
       <ul className="flex gap-5">
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/pairings">Pairings</a>
-        </li>
-        <li>
-          <a href="/sightings">Sightings</a>
-        </li>
-        <li>
-          <a href="/releases">Releases</a>
-        </li>
+        {navLinks.map((link) => (
+          <li key={link.name}>
+            <a href={link.href} className="group flex flex-col items-center">
+              {link.name}
+              <div className="bg-green-700 transition-transform duration-300 scale-x-0  group-hover:scale-x-100 h-1 w-full" />
+            </a>
+          </li>
+        ))}
       </ul>
     </nav>
   )
--- client/pages/Layout.tsx
+++ client/pages/Layout.tsx
@@ -3,11 +3,20 @@ import Nav from '../components/Nav'
 
 export default function Layout() {
   return (
-    <>
-      <Nav />
+    <div className="flex flex-col my-2">
+      <div className="flex justify-between items-center">
+        <div className=" w-20 h-20 rounded-[50%] mx-4 flex justify-center overflow-hidden">
+          <img
+            className="max-w-full max-h-full object-cover"
+            alt="kaki-logo"
+            src="client/assets/kakiLogo.png"
+          />
+        </div>
+        <Nav />
+      </div>
       <main>
         <Outlet />
       </main>
-    </>
+    </div>
   )
 }
